ba1fb05c3c165b8f8b0ff326ffbfa81b4f29c940
Dimpho Ramodise <dimpho.ramodise@gmail.com>
2019-07-29 18:37:59 +0200

    gnomio oop assignment

--- assignment3/compInt.spec.js
+++
@@ -1,9 +0,0 @@
-describe('BankAccount', () => {
-    let dice;
-    beforeEach(() => {
-        dice = new BankAccount();
-    })
-    it('should define the persons account information', () => {
-        expect(dice.accountInformation()).toBeDefined();
-    })
-});
